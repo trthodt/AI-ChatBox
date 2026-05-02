4b8f3a12dd29ce5caf9693c19ec4703f8bca9fa4
TribrigadeKits <174474576+TribrigadeKits@users.noreply.github.com>
2026-05-02 11:48:23 +0700

    feat: connect to db [TribrigadeKits]

--- chatbox_backend/main.py
+++ chatbox_backend/main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import chatbox_router
+from app.core.database import connect
+from app.schemas.user import User
 
 app = FastAPI()
 
+origin = [
+  'http://localhost:5173/*', 
+  'http://localhost:3000/*', 
+  'http://localhost:8000/*'
+]
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origin,
+  allow_credentials=True,
+  allow_headers=['*'],
+  allow_methods=['*'],
+)
+
+app.include_router(
+  chatbox_router.router,
+  prefix="/api"
+)
+
+connect()
 
 @app.get('/')
 def home():
